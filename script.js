7d91d148fd723e2d021d6d5ade5a6808f908ec35
louweal <anneloeslouwe@live.nl>
2025-10-21 12:23:57 +0200

    fix bug

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ app.post("/data", async (req, res) => {
 
         // get subscribers from database if missing (e.g. after server restart)
         if (!subscribers[topicId]) {
-            const subscribers = await fetchSensorSettings(topicId);
+            const subscribers = await fetchSensorSubscribers(topicId);
 
             // add all subscribers from database to subscribers object
             for (const subscriber of subscribers) {
